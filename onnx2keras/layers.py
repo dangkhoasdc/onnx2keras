6daead76fb44a75bf1462051fc05478bc1a7794c
Khoa Le Tan Dang <letan.dangkhoa@gmail.com>
2022-07-12 22:34:04 +0800

    Support HardSigmoid op

--- onnx2keras/layers.py
+++ onnx2keras/layers.py
@@ -1,6 +1,6 @@
 from .convolution_layers import convert_conv, convert_convtranspose
 from .activation_layers import convert_relu, convert_elu, convert_lrelu, convert_selu, \
-    convert_sigmoid, convert_tanh, convert_softmax, convert_prelu
+    convert_sigmoid, convert_tanh, convert_softmax, convert_prelu, convert_hard_sigmoid
 from .operation_layers import convert_clip, convert_exp, convert_reduce_sum, convert_reduce_mean, \
     convert_log, convert_pow, convert_sqrt, convert_split, convert_cast, convert_floor, convert_identity, \
     convert_argmax, convert_reduce_l2, convert_reduce_max
@@ -71,4 +71,5 @@ AVAILABLE_CONVERTERS = {
     'Flatten': convert_flatten,
     'Upsample': convert_upsample,
     'Resize': convert_upsample,
+    'HardSigmoid': convert_hard_sigmoid,
 }
