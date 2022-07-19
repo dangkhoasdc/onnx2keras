3adf8fe87c0a157f360d80dbbadab4d4d9ca8758
Khoa Le Tan Dang <letan.dangkhoa@gmail.com>
2022-07-19 16:03:35 +0800

    support test case for padding=same

--- test/layers/convolutions/test_conv2d.py
+++ test/layers/convolutions/test_conv2d.py
@@ -4,17 +4,17 @@ import pytest
 import tensorflow as tf
 
 from test.utils import convert_and_test
+from test.layers.convolutions.conv2d_utils import Conv2dSame
 
 
 class LayerTest(nn.Module):
     def __init__(self, inp, out, kernel_size=3, padding=1, stride=1, bias=False, dilation=1, groups=1, padding_mode='valid'):
         super(LayerTest, self).__init__()
         assert padding_mode in ['valid', 'same']
-
+        conv_module = nn.Conv2d 
         if padding_mode == 'same':
-            padding = 'same'
-
-        self.conv = nn.Conv2d(
+            conv_module = Conv2dSame
+        self.conv = conv_module(
             inp, out, kernel_size=kernel_size, padding=padding,
             stride=stride, bias=bias, dilation=dilation, groups=groups
         )
@@ -26,8 +26,10 @@ class LayerTest(nn.Module):
 
 def func(change_ordering, kernel_size, padding, stride, bias, dilation, groups, padding_mode):
     if not tf.test.gpu_device_name() and not change_ordering:
+        print("skip this 11")
         pytest.skip("Skip! Since tensorflow Conv2D op currently only supports the NHWC tensor format on the CPU")
     if stride > 1 and dilation > 1:
+        print("skip this 22")
         pytest.skip("strides > 1 not supported in conjunction with dilation_rate > 1")
     model = LayerTest(
         groups * 3, groups,
@@ -57,6 +59,6 @@ def test_conv2d_case1(change_ordering, kernel_size, padding, stride, bias, dilat
 @pytest.mark.parametrize('bias', [True, False])
 @pytest.mark.parametrize('dilation', [1])
 @pytest.mark.parametrize('groups', [1, 2, 3])
-@pytest.mark.parametrize('padding_mode', ['valid'])
+@pytest.mark.parametrize('padding_mode', ['valid', 'same'])
 def test_conv2d_case2(change_ordering, kernel_size, padding, stride, bias, dilation, groups, padding_mode):
     func(change_ordering, kernel_size, padding, stride, bias, dilation, groups, padding_mode)
